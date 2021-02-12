95e7848874f5f7bce9c3cb14012d66262dd00089
Artem <ufimzev.art@gmail.com>
2021-02-13 02:26:44 +0700

    Initial commit with layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
+import { TicketComponent } from './ticket/ticket.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    TicketListComponent,
+    TicketComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule
